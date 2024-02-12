f9ce83b621c58458a072595b40c2354b993925bf
Vyacheslav Scherbinin <gentslava@mail.ru>
2024-02-12 16:17:22 +0700

    Multiple Cron schedules bash command

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ npm run entrypoint-config
 TZ=$(cat $TEMP_CONFIG | jq -r ".timezone")
 RUN_ON_STARTUP=$(cat $TEMP_CONFIG | jq -r ".runOnStartup")
 RUN_ONCE=$(cat $TEMP_CONFIG | jq -r ".runOnce")
-CRON_SCHEDULE=$(cat $TEMP_CONFIG | jq -r ".cronSchedule")
+CRON_SCHEDULE=$(cat $TEMP_CONFIG | jq -r ".cronSchedule[]? // .cronSchedule")
 
 echo "Setting timezone: $TZ"
 ln -snf /usr/share/zoneinfo/"$TZ" /etc/localtime
@@ -24,17 +24,22 @@ fi
 # If runOnce is not set, schedule the process
 echo "Run once: ${RUN_ONCE}"
 if [ "$RUN_ONCE" = "false" ]; then
-    echo "Setting cron schedule as ${CRON_SCHEDULE}"
     if [ "$DISTRO" = "alpine" ]; then
         # Add the command to the crontab
-        echo "${CRON_SCHEDULE} cd /usr/app && node /usr/app/dist/src/index.js" | crontab -
+        echo "$CRON_SCHEDULE" | while read CRON_LINE ; do
+            echo "Setting cron schedule as ${CRON_LINE}"
+            echo "${CRON_LINE} cd /usr/app && node /usr/app/dist/src/index.js" | crontab -
+        done
         # Run the cron process. The container should halt here and wait for the schedule.
         /usr/sbin/crond -f -l 8
     else
         # Debian cron wipes the environment, so we save it to a script to load in cron
         printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
         # Add the command to the crontab. Debian cron doesn't ensure single instance, so we use flock to ensure it
-        echo "${CRON_SCHEDULE} . /root/project_env.sh && cd /usr/app && flock -n /var/lock/epicgames.lock node /usr/app/dist/src/index.js > /proc/1/fd/1 2>/proc/1/fd/2" | crontab -
+        echo "$CRON_SCHEDULE" | while read CRON_LINE ; do
+            echo "Setting cron schedule as ${CRON_LINE}"
+            echo "${CRON_LINE} . /root/project_env.sh && cd /usr/app && flock -n /var/lock/epicgames.lock node /usr/app/dist/src/index.js > /proc/1/fd/1 2>/proc/1/fd/2" | crontab -
+        done
         # Run the cron process. The container should halt here and wait for the schedule.
         cron -f
     fi
